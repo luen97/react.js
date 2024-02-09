43aeab90ca046fd6e59e0bc90cdb0728e3f0e019
Manuel Salazar <manalgom@gmail.com>
2024-02-09 15:14:33 -0500

    events and states

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,11 +1,18 @@
+import React from 'react';
 import './TodoSearch.css';
 
+
 function TodoSearch() {
-  return (
-    <input
-      placeholder="Cortar cebolla"
-      className="TodoSearch"
-    />
+   const [searchValue, setSearchValue] = React.useState('');
+   return (
+      <input
+         placeholder="Cortar cebolla"
+         className="TodoSearch"
+         value={searchValue}
+         onChange={(event) => {
+            setSearchValue(event.target.value);
+         }}
+      />
   );
 }
 
